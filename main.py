ff0301e00646d5e475a20292ba730ceb7eebeff9
aguiar44 <aguiar_lucasaguiar@hotmail.com>
2025-04-17 18:11:25 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+
 import os
 import telebot
-from telebot import types
 
 API_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
@@ -14,6 +14,15 @@ def gerar_video(message):
     bot.send_chat_action(message.chat.id, 'upload_video')
     bot.send_message(message.chat.id, "Gerando vídeo viral...")
 
-    bot.send_video(message.chat.id, open("video_infiltrador.mp4", 'rb'), caption="Você ainda acha que tem tempo?\n\nINFLITRADOS — A verdade não será gentil.")
+    try:
+        with open("video_infiltrador.mp4", "rb") as video:
+            bot.send_video(message.chat.id, video, caption="Você ainda acha que tem tempo?\n\nINFLITRADOS — A verdade não será gentil.")
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Ocorreu um erro ao enviar o vídeo: {e}")
 
-bot.polling()
+if __name__ == "__main__":
+    print("INFILTRADOR INICIADO. Aguardando comandos...")
+    try:
+        bot.polling(none_stop=True, interval=0, timeout=20)
+    except Exception as e:
+        print(f"Erro ao iniciar o bot: {e}")
